d887de39395d07a397b67548336e891c9e6a8e43
Przemysław Nikratowicz <pnikratowicz@prograils.com>
2018-01-23 00:08:06 +0100

    Add babelrc

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,19 +4,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import NotFound from '../../components/NotFound';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
