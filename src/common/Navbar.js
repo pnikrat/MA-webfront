bbbb6232fca61301f5731b715e12e4ca07c8e2b2
Przemek <sniffoza@gmail.com>
2018-02-15 13:50:53 +0100

    Style login form

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react';
+import { Icon, Menu } from 'semantic-ui-react';
 import { signOutUser } from '../auth-config';
 
 type Props = {
@@ -22,9 +22,13 @@ class Navbar extends Component<Props> {
     const { currentUser } = this.props;
     return (
       <Menu>
-        <Menu.Item as={Link} to="/">Home</Menu.Item>
+        <Menu.Item as={Link} to="/">
+          <Icon name="home" />
+          Home
+        </Menu.Item>
         { currentUser.isSignedIn &&
           <Menu.Item position="right" as={Link} to="/" onClick={this.handleSignOut}>
+            <Icon name="sign out" />
             Sign out
           </Menu.Item>
         }
