30b9fd5352f42951a5d10fe41a9625703329e640
Przemek <sniffoza@gmail.com>
2018-07-08 18:08:53 +0200

    Only list absolute creator can delete it

--- src/lists/Lists.js
+++ src/lists/Lists.js
@@ -4,17 +4,30 @@ import { Button, Container, Segment } from 'semantic-ui-react';
 
 type Props = {
   lists: Array<Object>,
+  currentUser: Object,
   onListClick: (Number) => void,
   openConfirmationModal: (Object, number) => void,
   openEditListModal: (Object, Object) => void,
 }
 
 class Lists extends Component<Props> {
+  compare = (x: Object, y: Object) => {
+    if (x.user_id === y.user_id) {
+      if (x.id < y.id) {
+        return -1;
+      }
+      return 1;
+    }
+    return x.user_id === this.props.currentUser.id ? -1 : 1;
+  }
+
+  isCreator = (creatorId: number) => this.props.currentUser.id === creatorId
+
   render() {
     const {
       lists, onListClick, openConfirmationModal, openEditListModal
     } = this.props;
-    const listsItems = lists.map(list =>
+    const listsItems = lists.sort(this.compare).map(list =>
       (
         <Segment
           key={list.id}
@@ -34,15 +47,17 @@ class Lists extends Component<Props> {
             >
               Edit
             </Button>
-            <Button
-              compact
-              color="red"
-              data-cy="delete-list-button"
-              onMouseDown={e => e.stopPropagation()}
-              onClick={e => openConfirmationModal(e, list.id)}
-            >
-              Delete
-            </Button>
+            { this.isCreator(list.user_id) &&
+              <Button
+                compact
+                color="red"
+                data-cy="delete-list-button"
+                onMouseDown={e => e.stopPropagation()}
+                onClick={e => openConfirmationModal(e, list.id)}
+              >
+                Delete
+              </Button>
+            }
           </div>
         </Segment>
       )
