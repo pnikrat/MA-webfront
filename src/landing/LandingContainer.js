3c78b1b777698c42c1e1e124a6c2be98655f9318
Przemek <sniffoza@gmail.com>
2018-07-31 23:07:17 +0200

    Create and style basic landing page

--- src/landing/LandingContainer.js
+++ src/landing/LandingContainer.js
@@ -1,14 +1,85 @@
 // @flow
 import React from 'react';
-import { Button, Container } from 'semantic-ui-react';
+import { Button, Container, Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import '../styles/landing.css';
 
 function LandingContainer() {
   return (
-    <Container>
-      <Button primary as={Link} to="/signup" data-cy="to-register-form-button">Rejestracja</Button>
-      <Button primary as={Link} to="/signin" data-cy="to-login-form-button">Logowanie</Button>
-    </Container>
+    <div>
+      <div className="jumbo">
+        <div className="jumbo-content">
+          <div className="jumbo-header">
+            <h1>
+              Grupowa lista zakupów
+            </h1>
+          </div>
+          <div className="jumbo-buttons-container">
+            <Button
+              primary
+              as={Link}
+              to="/signup"
+              data-cy="to-register-form-button"
+              className="jumbo-button"
+            >
+              Rejestracja
+            </Button>
+            <Button
+              primary
+              as={Link}
+              to="/signin"
+              data-cy="to-login-form-button"
+              className="jumbo-button"
+            >
+              Logowanie
+            </Button>
+          </div>
+        </div>
+      </div>
+      <Container>
+        <div className="feature-segment">
+          <Header>
+            Lorem Ipsum
+          </Header>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in rep
+          rehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+          </p>
+        </div>
+
+        <div className="feature-segment">
+          <Header>
+            Lorem Ipsum
+          </Header>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in rep
+          rehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+          </p>
+        </div>
+
+        <div className="feature-segment">
+          <Header>
+            Lorem Ipsum
+          </Header>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in rep
+          rehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+          </p>
+        </div>
+      </Container>
+    </div>
   );
 }
 
